9ba4435617f68cad4cf50865c951ecc690c6fc24
jackalot <cgonzalez50@bucs.fsw.edu>
2024-10-23 14:24:44 -0400

    created functiion validate as an onchange method
    as an on change method, the name field will now call Validate.

--- components/ui/nameField.jsx
+++ components/ui/nameField.jsx
@@ -2,6 +2,10 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
+function Validate () {
+    console.log("hi");
+    
+}
 const NameField = React.forwardRef(({ className, placeholder }, ref) => {
     return (
         (<input
