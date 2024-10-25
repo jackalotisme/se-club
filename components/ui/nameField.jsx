f47356ebde707962ca23e41d24777f07e563d7d2
jackalot <cgonzalez50@bucs.fsw.edu>
2024-10-25 07:33:39 -0400

    formatted NameField

--- components/ui/nameField.jsx
+++ components/ui/nameField.jsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils"
 import { Input, stringify } from "postcss";
 
 const NameField = React.forwardRef(({ className, placeholder }, ref) => {
-    function validateText (text) {
+    function validateText(text) {
         /*Only text, does not contain
          *  any numbers
          * special characters, ie: ! @ #"
@@ -12,76 +12,70 @@ const NameField = React.forwardRef(({ className, placeholder }, ref) => {
         let count = 0;
         let containsNonvalidChar = false;
         let keepGoing = true;
-        while(containsNonvalidChar == false && keepGoing == true)
-        {
-            if(parseInt(text[count]) >= 0 || text.length == 0)
-                {
-                    containsNonvalidChar = true;
-                }
-                else
-                {
-                    switch(text[count])
-                    {
-                        case"`":
-                        case"~":
-                        case"!":
-                        case"@":
-                        case"#":
-                        case"$":
-                        case"%":
-                        case"^":
-                        case"&":
-                        case"*":
-                        case"(":
-                        case")":
-                        case"-":
-                        case"_":
-                        case"+":
-                        case"=":
-                        case"{":
-                        case"[":
-                        case"}":
-                        case"]":
-                        case"\\":
-                        case"|":
-                        case":":
-                        case";":
-                        case'"':
-                        case"'":
-                        case"?":
-                        case"/":
-                        case">":
-                        case"<":
-                        case",":
-                        case".":
+        while (containsNonvalidChar == false && keepGoing == true) {
+            if (parseInt(text[count]) >= 0 || text.length == 0) {
+                containsNonvalidChar = true;
+            }
+            else {
+                switch (text[count]) {
+                    case "`":
+                    case "~":
+                    case "!":
+                    case "@":
+                    case "#":
+                    case "$":
+                    case "%":
+                    case "^":
+                    case "&":
+                    case "*":
+                    case "(":
+                    case ")":
+                    case "-":
+                    case "_":
+                    case "+":
+                    case "=":
+                    case "{":
+                    case "[":
+                    case "}":
+                    case "]":
+                    case "\\":
+                    case "|":
+                    case ":":
+                    case ";":
+                    case '"':
+                    case "'":
+                    case "?":
+                    case "/":
+                    case ">":
+                    case "<":
+                    case ",":
+                    case ".":
                         containsNonvalidChar = true;
-                    }
-                }   
-                if(count > text.length)
-                {
-                    keepGoing = false;
                 }
-                count++;
+            }
+            if (count > text.length) {
+                keepGoing = false;
+            }
+            count++;
         }
-        if(containsNonvalidChar)
-        {
+        if (containsNonvalidChar) {
             console.log("invalid character fix that!");
         }
     }
-    function handleChange (e) {
+    function handleChange(e) {
         let text = e.target.value;
         validateText(text);
     }
     return (
         (<input
-          type="text"
-          className={cn(
-            "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-            className
-          )}
-          ref={ref}
-          placeholder={placeholder}
-          onChange={handleChange} />)
+            type="text"
+            className={cn(
+                "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+                className
+            )}
+            ref={ref}
+            placeholder={placeholder}
+            onChange={handleChange} />)
     );
 })
 
