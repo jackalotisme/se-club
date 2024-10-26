3f1e60ff4cf441acf5245e829ed3ae07aec32f0e
jackalot <cgonzalez50@bucs.fsw.edu>
2024-10-25 22:18:06 -0400

    added cases to checkValidName

--- components/ui/emailField.jsx
+++ components/ui/emailField.jsx
@@ -55,13 +55,185 @@ const EmailField = React.forwardRef(({ className, placeholder }, ref) => {
         return containsNonvalidChar;
     }
     function checkValidName(text) {
+        let valid = false;
         switch (text) {
-            case "":
+            case "a":
+                valid = true;
+                break;
+            case "b":
+                valid = true;
+                break;
+            case "c":
+                valid = true;
+                break;
+            case "d":
+                valid = true;
+                break;
+            case "e":
+                valid = true;
+                break;
+            case "f":
+                valid = true;
+                break;
+            case "g":
+                valid = true;
+                break;
+            case "h":
+                valid = true;
+                break;
+            case "i":
+                valid = true;
+                break;
+            case "j":
+                valid = true;
+                break;
+            case "k":
+                valid = true;
+                break;
+            case "l":
+                valid = true;
+                break;
+            case "m":
+                valid = true;
+                break;
+            case "n":
+                valid = true;
+                break;
+            case "o":
+                valid = true;
+                break;
+            case "p":
+                valid = true;
+                break;
+            case "q":
+                valid = true;
+                break;
+            case "r":
+                valid = true;
+                break;
+            case "s":
+                valid = true;
+                break;
+            case "t":
+                valid = true;
+                break;
+            case "u":
+                valid = true;
+                break;
+            case "v":
+                valid = true;
+                break;
+            case "w":
+                valid = true;
+                break;
+            case "x":
+                valid = true;
+                break;
+            case "y":
+                valid = true;
+                break;
+            case "z":
+                valid = true;
+                break;
+            case "1":
+                valid = true;
+                break;
+            case "2":
+                valid = true;
+                break;
+            case "3":
+                valid = true;
+                break;
+            case "4":
+                valid = true;
+                break;
+            case "5":
+                valid = true;
+                break;
+            case "6":
+                valid = true;
+                break;
+            case "7":
+                valid = true;
+                break;
+            case "8":
+                valid = true;
+                break;
+            case "9":
+                valid = true;
+                break;
+            case "0":
+                valid = true;
+                break;
+            case ".":
+                valid = true;
+                break;
+            case "?":
+                valid = true;
+                break;
+            case "!":
+                valid = true;
+                break;
+            case "{":
+                valid = true;
+                break;
+            case "}":
+                valid = true;
+                break;
+            case "~":
+                valid = true;
+                break;
+            case "-":
+                valid = true;
+                break;
+            case "_":
+                valid = true;
+                break;
+            case "+":
+                valid = true;
+                break;
+            case "/":
+                valid = true;
+                break;
+            case "=":
+                valid = true;
+                break;
+            case "%":
+                valid = true;
+                break;
+            case "$":
+                valid = true;
+                break;
+            case "#":
+                valid = true;
+                break;
+            case "$":
+                valid = true;
+                break;
+            case "#":
+                valid = true;
+                break;
+            case "'":
+                valid = true;
+                break;
+            case "`":
+                valid = true;
+                break;
+            case "|":
+                valid = true;
+                break;
+            case "^":
+                valid = true;
+                break;
+            case "*":
+                valid = true;
                 break;
             default:
+                valid = false;
                 break;
         }
     }
+}
     function validateText(text) {
         let result = checkInvalidCharacters(text);
         if (result == false) {
@@ -69,7 +241,6 @@ const EmailField = React.forwardRef(({ className, placeholder }, ref) => {
             //https://ladedu.com/valid-characters-for-email-addresses-the-complete-list/#Characters-Special-Characters-and-Symbols-Allowed-in-an-Email-Address
             let test = text.split("@");
             console.log(test);
-            result = checkValidName(test[0]);
             if (result == true) {
                 console.log("its valid")
             }
@@ -80,16 +251,16 @@ const EmailField = React.forwardRef(({ className, placeholder }, ref) => {
         validateText(text);
     }
     return (
-        (<input
-            type="email"
-            className={cn(
-                "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-                className
-            )}
-            ref={ref}
-            placeholder={placeholder}
-            onChange={handleChange} />)
-    );
+    (<input
+        type="email"
+        className={cn(
+            "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+            className
+        )}
+        ref={ref}
+        placeholder={placeholder}
+        onChange={handleChange} />)
+);
 })
 
 export { EmailField }
