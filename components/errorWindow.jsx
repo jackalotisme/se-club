d3d10274855eab4b71a109225062059cd3c266d2
jackalot <cgonzalez50@bucs.fsw.edu>
2024-10-26 08:47:23 -0400

    created ErrrorWindow file

---
+++ components/errorWindow.jsx
@@ -0,0 +1,25 @@
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+import { Input, stringify } from "postcss";
+
+const ErrorWindow = React.forwardRef(({ className, placeholder }, ref) => {
+
+    function handleChange(e) {
+        let text = e.target.value;
+        validateText(text);
+    }
+    return (
+        (<input
+            type="text"
+            className={cn(
+                "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+                className
+            )}
+            ref={ref}
+            placeholder={placeholder}
+            onChange={handleChange} />)
+    );
+})
+
+export { ErrorWindow }
