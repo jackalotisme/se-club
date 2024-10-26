3d42e45f9c2bc84500683b08672778609016dc05
jackalot <cgonzalez50@bucs.fsw.edu>
2024-10-26 10:08:16 -0400

    changed props for Error Window
    changed the props from className, placeholder to ErrorTitle, ErorrDescription

--- components/errorWindow.jsx
+++ components/errorWindow.jsx
@@ -3,13 +3,12 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 import { Input, stringify } from "postcss";
 
-const ErrorWindow = React.forwardRef(({ className, placeholder }, ref) => {
+const ErrorWindow = React.forwardRef(({ ErrorTitle, ErrorDescription }, ref) => {
 
     function handleChange(e) {
         let text = e.target.value;
     }
     return (
-        ()
     );
 })
 
