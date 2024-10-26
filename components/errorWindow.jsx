58e394be959a8f74cce979da33fa38a1d517960b
jackalot <cgonzalez50@bucs.fsw.edu>
2024-10-26 19:14:41 -0400

    Removed the button and DisableAlert function

--- components/errorWindow.jsx
+++ components/errorWindow.jsx
@@ -4,13 +4,6 @@ import { cn } from "@/lib/utils"
 import { Input, stringify } from "postcss";
 
 const ErrorWindow = React.forwardRef(({ ErrorTitle, ErrorDescription }, ref) => {
-    function DisableAlert(e) {
-        let element = e.target.parentElement.parentElement;
-        if (element.role === "alert") {
-            element.display = "none";
-        }
-        element.remove();
-    }
     if (ErrorTitle.length > 0) {
         return (
             (
@@ -18,7 +11,6 @@ const ErrorWindow = React.forwardRef(({ ErrorTitle, ErrorDescription }, ref) => 
                     <strong class="font-bold">{ErrorTitle}</strong>
                     <span class="block:inline">{ErrorDescription}</span>
                     <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-                        <svg class="fill-current h-6 w-6 text-red-500" role="button" onClick={DisableAlert} viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
                     </span>
                 </div>
             )
