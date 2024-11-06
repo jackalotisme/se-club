691267dcf4f555ab58fe4f63d20fe7a9c1442f40
jackalot <cgonzalez50@bucs.fsw.edu>
2024-11-05 20:16:04 -0500

    imported state into SignUpForm

--- components/forms/SignUpForm.jsx
+++ components/forms/SignUpForm.jsx
@@ -1,23 +1,5 @@
-/*<div className="w-full max-w-sm space-y-2">
-                  <form className="flex flex-col space-y-4">
-                    <NameField
-                      changeParentState={changeState}
-                      placeholder="Your Name"
-                      required
-                    ></NameField>
-                    <EmailField
-                      changeParentState={changeState}
-                      placeholder="Your Email"
-                      required
-                    ></EmailField>
-                    <Button type="submit" id="SignUp">
-                      Sign Up
-                    </Button>
-                  </form>
-                </div> */
-
 import * as React from "react";
-
+import { useState } from "react";
 import { cn } from "@/lib/utils"
 
 const SignUpForm = React.forwardRef((ref) => {
