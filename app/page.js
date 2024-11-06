a74cf610d7a9c6cf1548ad4eade09012481d00f5
jackalot <cgonzalez50@bucs.fsw.edu>
2024-11-05 20:39:34 -0500

    Removed the form to be replaced with SignUpForm

--- app/page.js
+++ app/page.js
@@ -182,23 +182,7 @@ export default function Component() {
                   engineering.
                 </p>
               </div>
-              <div className="w-full max-w-sm space-y-2">
-                <form className="flex flex-col space-y-4">
-                  <NameField
-                    changeParentState={changeState}
-                    placeholder="Your Name"
-                    required
-                  ></NameField>
-                  <EmailField
-                    changeParentState={changeState}
-                    placeholder="Your Email"
-                    required
-                  ></EmailField>
-                  <Button type="submit" id="SignUp">
-                    Sign Up
-                  </Button>
-                </form>
-              </div>
+              <SignUpForm></SignUpForm>
             </div>
           </div>
         </section>
