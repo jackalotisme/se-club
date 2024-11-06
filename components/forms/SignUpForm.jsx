c60b203b85c15a649a56c51b3a1cd1961994d619
jackalot <cgonzalez50@bucs.fsw.edu>
2024-11-05 21:03:07 -0500

    formatting

--- components/forms/SignUpForm.jsx
+++ components/forms/SignUpForm.jsx
@@ -15,23 +15,24 @@ const SignUpForm = React.forwardRef((placeHolder, ref) => {
         }
     }
     return (
-        (<div className="w-full max-w-sm space-y-2">
-            <form className="flex flex-col space-y-4">
-                <NameField
-                    changeParentState={changeState}
-                    placeholder="Your Name"
-                    required
-                ></NameField>
-                <EmailField
-                    changeParentState={changeState}
-                    placeholder="Your Email"
-                    required
-                ></EmailField>
-                <Button type="submit" id="SignUp">
-                    Sign Up
-                </Button>
-            </form>
-        </div>)
+        (
+            <div className="w-full max-w-sm space-y-2">
+                <form className="flex flex-col space-y-4">
+                    <NameField
+                        changeParentState={changeState}
+                        placeholder="Your Name"
+                        required
+                    ></NameField>
+                    <EmailField
+                        changeParentState={changeState}
+                        placeholder="Your Email"
+                        required
+                    ></EmailField>
+                    <Button type="submit" id="SignUp">
+                        Sign Up
+                    </Button>
+                </form>
+            </div>)
     );
 })
 
