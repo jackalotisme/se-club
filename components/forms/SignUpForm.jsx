55d61b6d9d180be3db1788ec8cd8d2911783e970
jackalot <cgonzalez50@bucs.fsw.edu>
2024-11-05 20:13:45 -0500

    removed props, added return statement

--- components/forms/SignUpForm.jsx
+++ components/forms/SignUpForm.jsx
@@ -20,8 +20,26 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils"
 
-const SignUpForm = React.forwardRef(({ className, ...props }, ref)) {
-    //return("");
-}
+const SignUpForm = React.forwardRef((ref) => {
+    return (
+        (<div className="w-full max-w-sm space-y-2">
+            <form className="flex flex-col space-y-4">
+                <NameField
+                    changeParentState={changeState}
+                    placeholder="Your Name"
+                    required
+                ></NameField>
+                <EmailField
+                    changeParentState={changeState}
+                    placeholder="Your Email"
+                    required
+                ></EmailField>
+                <Button type="submit" id="SignUp">
+                    Sign Up
+                </Button>
+            </form>
+        </div>)
+    );
+})
 
 export { SignUpForm }
