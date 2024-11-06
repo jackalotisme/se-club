024e28a38851bfd1135c43a16b5cac5411c7554b
jackalot <cgonzalez50@bucs.fsw.edu>
2024-11-05 19:58:48 -0500

    added a comment to later copy from
    added the current div that stores the current form elements all into the new sign up form.

---
+++ components/forms/SignUpForm.jsx
@@ -0,0 +1,17 @@
+/*<div className="w-full max-w-sm space-y-2">
+                  <form className="flex flex-col space-y-4">
+                    <NameField
+                      changeParentState={changeState}
+                      placeholder="Your Name"
+                      required
+                    ></NameField>
+                    <EmailField
+                      changeParentState={changeState}
+                      placeholder="Your Email"
+                      required
+                    ></EmailField>
+                    <Button type="submit" id="SignUp">
+                      Sign Up
+                    </Button>
+                  </form>
+                </div> */
