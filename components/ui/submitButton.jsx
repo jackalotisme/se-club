808d9ab14b2f3a9d712cc83abeaf31c42b0794b2
jackalot <cgonzalez50@bucs.fsw.edu>
2024-11-11 15:10:57 -0500

    created submitButton file/component

---
+++ components/ui/submitButton.jsx
@@ -0,0 +1,23 @@
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+import { useState } from "react";
+
+const SubmitButton = React.forwardRef(({ className, placeholder, changeParentState }, ref) => {
+
+    return (
+        (<><input
+            type="submit"
+            className={cn(
+                "flex h-9 w-full rounded-md border border-input bg-white px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+                className
+            )}
+            ref={ref}
+            id={"SignUp"}
+        />
+        </>
+        )
+    );
+})
+
+export { SubmitButton }
