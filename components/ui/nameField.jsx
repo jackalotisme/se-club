e327ac49bb5b1bc4f4e5d6d3c3c14b02202bffed
jackalot <cgonzalez50@bucs.fsw.edu>
2024-11-11 14:05:10 -0500

    Removed Input component in return statement
    The summary is, the input component would eat our onChange calls as a prop from NameField. We had to remove Input to prevent that.

--- components/ui/nameField.jsx
+++ components/ui/nameField.jsx
@@ -83,17 +83,19 @@ const NameField = React.forwardRef(({ className, placeholder, changeParentState 
     }
     function handleChange(e) {
         let text = e.target.value;
+        console.log("this changed")
         validateText(text);
     }
     return (
-        (<><Input
-            type="text"
-            className={cn(
-                "flex h-9 w-full rounded-md border border-input bg-white px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50", className
-            )}
-            ref={ref}
-            placeholder={placeholder}
-            onChange={handleChange} />
+        (<>
+            <input
+                type="text"
+                className={cn(
+                    "flex h-9 w-full rounded-md border border-input bg-white px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50", className
+                )}
+                ref={ref}
+                placeholder={placeholder}
+                onChange={handleChange} />
             <ErrorWindow ErrorTitle={errorType}
                 ErrorDescription={errorDescription}
             ></ErrorWindow>
