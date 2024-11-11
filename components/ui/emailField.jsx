a616d549f4f152b90e0b319b5140736432709416
jackalot <cgonzalez50@bucs.fsw.edu>
2024-11-11 14:26:39 -0500

    replaced input component with a input element

--- components/ui/emailField.jsx
+++ components/ui/emailField.jsx
@@ -279,7 +279,7 @@ const EmailField = React.forwardRef(({ className, placeholder, changeParentState
         validateText(text);
     }
     return (
-        (<><Input
+        (<><input
             type="email"
             className={cn(
                 "flex h-9 w-full rounded-md border border-input bg-white px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
@@ -287,7 +287,9 @@ const EmailField = React.forwardRef(({ className, placeholder, changeParentState
             )}
             ref={ref}
             placeholder={placeholder}
-            onChange={handleChange} />
+            onChange={handleChange}
+        />
+
             <ErrorWindow ErrorTitle={errorType}
                 ErrorDescription={errorDescription}></ErrorWindow>
         </>
