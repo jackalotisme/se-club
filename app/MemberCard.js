c125d0770be6149f9d99bac217ae43bf632d4063
Melissa Elizondo <melissaelizn@gmail.com>
2024-11-12 12:27:02 -0500

    minor changes

--- app/MemberCard.js
+++ app/MemberCard.js
@@ -5,16 +5,18 @@ function MemberCard() {
 
   const seedMemberData = [
     {id: 1, name: 'Melissa Elizondo', bio: '☀️♈ ● 🌙♈ ● ⬆️♊ ● ENTP ● I like coding :p ', image: 'https://i.pinimg.com/enabled_hi/564x/64/8b/bf/648bbfd67197bd5d2beacd9de37f608d.jpg' },
-    {id: 1, name: 'Elissa Ayalem Fonseca Saybe', bio: '(っ◔◡◔)っ ♡ it’s a perfect love ♡ she/they', image: 'https://pbs.twimg.com/media/GZ7_h9nb0AIg_It?format=jpg&name=4096x4096' },
-    {id: 1, name: 'Kiana Leal', bio: "yup this is the bio, that's it" , image: 'https://i.pinimg.com/736x/1b/6c/dc/1b6cdcb0ca861bd72c795a165e29247c.jpg' }
+    {id: 2, name: 'Elissa Ayalem Fonseca Saybe', bio: '(っ◔◡◔)っ ♡ it’s a perfect love ♡ she/they', image: 'https://pbs.twimg.com/media/GZ7_h9nb0AIg_It?format=jpg&name=4096x4096' },
+    {id: 3, name: 'Kiana Leal', bio: "yup this is the bio, that's it" , image: 'https://i.pinimg.com/736x/1b/6c/dc/1b6cdcb0ca861bd72c795a165e29247c.jpg' },
   ]
 
   const populateMembers = seedMemberData.map((member) => 
     <div>
       <div class="p-2">
-      <img class="h-64 w-64 rounded-full object-cover mx-auto" src= {member.image}></img>
-      <p> {member.name} </p>
+        <div class="content-center ">
+      <img class="h-44 w-44 rounded-full object-cover mx-auto" src= {member.image}></img>
+      <p class="text-center p-4" > {member.name} </p>
       </div>
+        </div>
     </div>
   )
 
@@ -22,9 +24,11 @@ function MemberCard() {
     <section id="events" className="w-full py-12 md:py-24 lg:py-32">
 
     <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl text-center mb-8">Members</h2>
-      <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-    {populateMembers}
-      </div>
+      {/* <div class="self-auto"> */}
+      <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 content-center">
+      {populateMembers}
+      {/* </div> */}
+    </div>
     </section>
   )
 }
--- app/page.js
+++ app/page.js
@@ -9,13 +9,15 @@ import { Code, Globe, Users } from "lucide-react"
 import Link from "next/link"
 
 import MemberCard from "@/app/MemberCard"
+import HeroSection from "@/components/ui/HeroSection";
 
 export default function Component() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-1 bg-gradient-to-tr from-sky-500 to-violet-700">
-        <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
+{/* Start of Hero Section */}
+        {/* <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
           <div className="container px-4 md:px-6 mx-auto">
             <div className="flex flex-col items-center space-y-4 text-center">
               <div className="space-y-2">
@@ -32,7 +34,9 @@ export default function Component() {
               </div>
             </div>
           </div>
-        </section>
+        </section> */}
+<HeroSection></HeroSection>
+{/* End of Hero Section */}
         <section id="about" className="w-full py-12 md:py-24 lg:py-32 bg-gray-100 dark:bg-gray-800">
           <div className="container px-4 md:px-6 mx-auto">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl text-center mb-8">About Us</h2>
