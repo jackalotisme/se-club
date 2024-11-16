1deb1d82b4635baa002e1ff307e0025eb9e75da1
Adam <devadamstandish@gmail.com>
2024-11-16 17:14:44 -0500

    Update submitButton.jsx
    changed input to button, imported button component

--- components/ui/submitButton.jsx
+++ components/ui/submitButton.jsx
@@ -2,11 +2,12 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
 const SubmitButton = React.forwardRef(({ className, placeholder, canSubmit }, ref) => {
     if (canSubmit) {
         return (
-            (<><input
+            (<><button
                 type="submit"
                 className={cn(
                     "flex h-9 w-full rounded-md border border-input bg-white px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
@@ -16,7 +17,7 @@ const SubmitButton = React.forwardRef(({ className, placeholder, canSubmit }, re
                 ref={ref}
                 id={"SignUp"}
                 disabled={false}
-            />
+                >Submit</button>
             </>
             )
         );
