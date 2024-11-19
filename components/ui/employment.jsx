5ca9c1ea9653b708f42b9718c9f178df735322e7
cloudflips32 <devadamstandish@gmail.com>
2024-11-19 00:01:27 -0500

    id added, employment section

--- components/ui/employment.jsx
+++ components/ui/employment.jsx
@@ -84,7 +84,7 @@ const Employment = () => {
   const randomCompanies = getRandomCompanies(5);
 
   return (
-    <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-100 dark:bg-gray-800">
+    <section id="employment" className="w-full py-12 md:py-24 lg:py-32 bg-gray-100 dark:bg-gray-800">
       <div className="container mx-auto px-4">
         <h2 className="text-3xl md:text-5xl sm:text-4xl font-bold text-center mb-8">Several Potential Employers</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8">
