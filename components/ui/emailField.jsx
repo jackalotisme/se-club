9ca2e23790570dbb93fc469a74f0463160de2d34
Adam <devadamstandish@gmail.com>
2024-12-01 21:43:58 -0500

    Update emailField.jsx- Z-Index
    Z-index was necessary due to bg-video addition

--- components/ui/emailField.jsx
+++ components/ui/emailField.jsx
@@ -289,7 +289,7 @@ const EmailField = React.forwardRef(({ className, placeholder, changeParentState
         (<><input
             type="email"
             className={cn(
-                "flex h-9 w-full rounded-md border border-input bg-white px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+                "flex z-10 h-9 w-full rounded-md border border-input bg-white px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
                 className
             )}
             ref={ref}
