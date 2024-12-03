b2ab7a1c9a2f928fffbd7ec5ce9579d21a96cf6e
jackalot <cgonzalez50@bucs.fsw.edu>
2024-12-03 17:13:57 -0500

    the background of our errorwindow is now gray

--- components/ui/errorWindow.jsx
+++ components/ui/errorWindow.jsx
@@ -5,7 +5,7 @@ const ErrorWindow = React.forwardRef(({ ErrorTitle, ErrorDescription }, ref) => 
     if (ErrorTitle.length > 0) {
         return (
             (
-                <div className={cn("flex flex-col bg-transparent border-4 border-rose-500/100 border-radius: 15px text-red-700 px-4 py-3 rounded relative")} role="alert">
+                <div className={cn("flex flex-col bg-gray-500 border-4 border-rose-500/100 border-radius: 15px text-red-700 px-4 py-3 rounded relative")} role="alert">
                     <strong className={cn("font-bold")}>{ErrorTitle}</strong>
                     <span className={cn("block:inline")}>{ErrorDescription}</span>
                     <span className={cn("absolute top-0 bottom-0 right-0 px-4 py-3")}>
