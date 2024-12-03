1590e8bbd77278bb089843704bc7323aadfd17b9
Adam <devadamstandish@gmail.com>
2024-12-03 18:19:38 -0500

    Update HeroSection.jsx - Tab Shuffle

--- components/ui/HeroSection.jsx
+++ components/ui/HeroSection.jsx
@@ -40,19 +40,19 @@ function HeroSection() {
   return (
     <div>
       <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 relative overflow-hidden">
-      <div className='matrixs '>
-        <MatrixEffect particle={50} />
-      </div>    
-    <div className="container px-4 md:px-6 mx-auto">
-      <div className="flex flex-col items-center space-y-4 text-center">
-        <div className="space-y-2">
-          <h1 
-            className="text-3xl text-white 
-            font-bold tracking-tighter 
-            sm:text-4xl md:text-5xl lg:text-6xl/none
-            border w-fit bg-gradient-to-r blur-xl absolute">
-            FSW's Software Engineering Club
-          </h1>
+        <div className='matrixs '>
+          <MatrixEffect particle={50} />
+        </div>    
+        <div className="container px-4 md:px-6 mx-auto">
+          <div className="flex flex-col items-center space-y-4 text-center">
+            <div className="space-y-2">
+              <h1 
+                className="text-3xl text-white 
+                font-bold tracking-tighter 
+                sm:text-4xl md:text-5xl lg:text-6xl/none
+                border w-fit bg-gradient-to-r blur-xl absolute">
+                FSW's Software Engineering Club
+              </h1>
               <h1 
                 className="text-3xl text-white 
                 font-bold tracking-tighter 
