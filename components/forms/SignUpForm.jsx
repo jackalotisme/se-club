3c62c7e0c6101901b4437a8a897357ee442be786
jackalot <cgonzalez50@bucs.fsw.edu>
2024-12-04 13:30:25 -0500

    created a red reset button
    created a red reset button based off the SubmitButton.

--- components/forms/SignUpForm.jsx
+++ components/forms/SignUpForm.jsx
@@ -45,7 +45,11 @@ const SignUpForm = React.forwardRef((placeHolder, ref) => {
                         placeholder="Your Email"
                         required
                     ></EmailField>
-
+                    <input
+                        className={cn(
+                            "flex h-9 w-full rounded-md border border-input mx-auto bg-red-500 px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+                        )}
+                        type="reset" value="Reset"></input>
                     <SubmitButton
                         canSubmit={compareBoth}>Submit</SubmitButton>
                 </form>
