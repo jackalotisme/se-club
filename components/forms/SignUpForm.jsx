b1f8d93b6056fca68895dc23765e27a5220fef00
jackalot <cgonzalez50@bucs.fsw.edu>
2024-12-04 13:47:25 -0500

    reset button now calls reset state
    created function called ResetState(), reset button calls it. This is so when we want to reset, the submit button doesn't stay.

--- components/forms/SignUpForm.jsx
+++ components/forms/SignUpForm.jsx
@@ -31,6 +31,12 @@ const SignUpForm = React.forwardRef((placeHolder, ref) => {
             setValidEmail(false);
         }
     }
+    // For the reset button!
+    function ResetState() {
+        setValidName(false);
+        setValidEmail(false);
+    }
+
     return (
         (
             <div className="w-full max-w-sm space-y-2">
@@ -49,7 +55,8 @@ const SignUpForm = React.forwardRef((placeHolder, ref) => {
                         className={cn(
                             "flex h-9 w-full rounded-md border border-input mx-auto bg-red-500 px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
                         )}
-                        type="reset" value="Reset"></input>
+                        type="reset" value="Reset"
+                        onClick={ResetState}></input>
                     <SubmitButton
                         canSubmit={compareBoth}>Submit</SubmitButton>
                 </form>
