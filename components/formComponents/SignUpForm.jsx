39ee2f2c8cac0c134ac317cb2751c0ca6af59e1a
cloudflips32 <devadamstandish@gmail.com>
2024-12-05 23:02:08 -0500

    tailwind cleanup, button text, code tabbed to 2 spaces

--- components/formComponents/SignUpForm.jsx
+++ components/formComponents/SignUpForm.jsx
@@ -7,65 +7,71 @@ import { Button } from "../ui/button";
 import { SubmitButton } from "./submitButton";
 
 const SignUpForm = React.forwardRef((placeHolder, ref) => {
-    const [ValidName, setValidName] = useState(false);
-    const [ValidEmail, setValidEmail] = useState(false);
-    const [GetSubmitStatus, setSubmitStatus] = useState(false);
+  const [ValidName, setValidName] = useState(false);
+  const [ValidEmail, setValidEmail] = useState(false);
+  const [GetSubmitStatus, setSubmitStatus] = useState(false);
 
-    function changeNameState(TrueOrFalse) {
-        if (TrueOrFalse === true) {
-            setValidName(true);
-        } else {
-            setValidName(false);
-        }
+  function changeNameState(TrueOrFalse) {
+    if (TrueOrFalse === true) {
+      setValidName(true);
+    } else {
+      setValidName(false);
     }
-    function compareBoth() {
-        let result = false;
-        if (ValidEmail === true && ValidName === true) {
-            result = true;
-        }
-        return result;
+  }
+
+  function compareBoth() {
+    let result = false;
+    if (ValidEmail === true && ValidName === true) {
+      result = true;
     }
-    function changeEmailState(TrueOrFalse) {
-        if (TrueOrFalse === true) {
-            setValidEmail(true);
-        } else {
-            setValidEmail(false);
-        }
+    return result;
+  }
+
+  function changeEmailState(TrueOrFalse) {
+    if (TrueOrFalse === true) {
+      setValidEmail(true);
+    } else {
+      setValidEmail(false);
     }
-    // For the reset button!
-    function ResetState() {
-        setValidName(false);
-        setValidEmail(false);
-    }
+  }
 
-    return (
-        (
-            <div className="w-full max-w-sm space-y-2">
-                <form className="flex flex-col space-y-4">
-                    <NameField
-                        changeParentState={changeNameState}
-                        placeholder="Your Name"
-                        required
-                    ></NameField>
-                    <EmailField
-                        changeParentState={changeEmailState}
-                        placeholder="Your Email"
-                        required
-                    ></EmailField>
+  // For the reset button!
+  function ResetState() {
+    setValidName(false);
+    setValidEmail(false);
+  }
 
-                    <Button
-                        className={cn(
-                            "flex h-9 w-full rounded-md border border-input mx-auto bg-red-500 px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
-                        )}
-                        type="reset" value="Reset"
-                        onClick={ResetState}>
-                    </Button>
+  return (
+    <div className="w-full max-w-sm space-y-2">
+      <form className="flex flex-col space-y-4">
+        <NameField
+          changeParentState={changeNameState}
+          placeholder="Your Name"
+          required
+        ></NameField>
+        <EmailField
+          changeParentState={changeEmailState}
+          placeholder="Your Email"
+          required
+        ></EmailField>
 
-                    <SubmitButton
-                        canSubmit={compareBoth}>Submit</SubmitButton>
-                </form>
-            </div>)
-    );
-})
+        <Button
+          className={cn(
+            "flex h-9 w-60 rounded-md border border-input mx-auto bg-red-500 px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+          )}
+          type="reset"
+          value="Reset"
+          onClick={ResetState}
+        >
+          Reset
+        </Button>
 
-export { SignUpForm }
+        <SubmitButton canSubmit={compareBoth}>
+          Submit
+        </SubmitButton>
+      </form>
+    </div>
+  );
+});
+
+export { SignUpForm };
