ea8d991b21332250a18707851ea2b1715656468e
cloudflips32 <devadamstandish@gmail.com>
2024-12-06 00:15:16 -0500

    button sizes, submit button materializes and creates button row

--- components/formComponents/SignUpForm.jsx
+++ components/formComponents/SignUpForm.jsx
@@ -54,21 +54,21 @@ const SignUpForm = React.forwardRef((placeHolder, ref) => {
           placeholder="Your Email"
           required
         ></EmailField>
-
-        <Button
-          className={cn(
-            "flex h-9 w-60 rounded-md border border-input mx-auto bg-red-500 px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
-          )}
-          type="reset"
-          value="Reset"
-          onClick={ResetState}
-        >
-          Reset
-        </Button>
-
-        <SubmitButton canSubmit={compareBoth}>
-          Submit
-        </SubmitButton>
+        <div className="flex flex-row space-x-4 mx-auto">
+          <SubmitButton canSubmit={compareBoth}>
+            Submit
+          </SubmitButton>
+          <Button
+            className={cn(
+              "flex h-9 w-40 rounded-md border border-input mx-auto bg-red-500 px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+            )}
+            type="reset"
+            value="Reset"
+            onClick={ResetState}
+          >
+            Reset
+          </Button>
+        </div>
       </form>
     </div>
   );
